c186d1d1c1ad986ce8890c81a8f226c065fc7edf
Jordan Sookikian <jsookikian@gmail.com>
2018-05-22 20:49:48 -0700

    refactored file locations

--- app/utilities.py
+++
@@ -1,7 +0,0 @@
-def getValidatedUrl(url):
-    if 'http://' not in url:
-        return 'http://' + url
-    elif 'https://' not in url:
-        return url.replace("https://", "http://")
-    else:
-        return url
